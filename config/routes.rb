d466f4e33a54264565c45277c40d6297d4e7befc
Asit <asitshah93@gmail.com>
2016-12-01 18:26:02 +0000

    Alex's addition

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root 'reportnumbers#new'
   resources :reportnumbers
   resources :searches
   # The priority is based upon order of creation: first created -> highest priority.
